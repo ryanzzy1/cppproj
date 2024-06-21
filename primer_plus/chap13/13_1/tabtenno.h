2f698173b9458a1e3b61e887dedf193be84c7b26
ryan <ryan_zhang310@163.com>
2024-06-21 23:04:17 +0800

    add 12_8 and 13_1 folder.

---
+++ primer_plus/chap13/13_1/tabtenno.h
@@ -0,0 +1,21 @@
+// tabtenno.h -- a table-tennis base class
+#ifndef TABTENNO_H_
+#define TABTENNO_H_
+#include<string>
+using std::string;
+//simple base class
+class TableTennisPlayer
+{
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    TableTennisPlayer(const string & fn = "none",
+                      const string & ln = "none", bool ht = false);
+    void Name() const;
+    bool HasTable() const {return hasTable; };
+    void ResetTable(bool v) {hasTable = v;};
+};
+
+#endif
