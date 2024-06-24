b8a8ffdc0cfaf36c550c210a36af1aab5def94b6
ryan <ryan_zhang310@163.com>
2024-06-24 09:44:07 +0800

    add file operation

---
+++ other/filesys_write.cpp
@@ -0,0 +1,25 @@
+// using <filesystem> library to write file
+// since c++ 17 statdard
+
+
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesytem;
+
+int main() {
+    // create file path
+    fs::path myFile("myfile1.txt");
+
+    // open file and write text 
+    fs::ofstream ofs(myFile);
+    if (ofs.is_open()) {
+        ofs << "This is a line of text in my file." << endl;
+        ofs.close();
+    } else {
+        cout << "Error opening file." << endl;
+    }
+
+    return 0;
+}
