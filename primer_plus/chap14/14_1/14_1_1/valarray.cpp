918a57cfab807e6c2741f03cfe2470fb00160334
ryan <ryan_zhang310@163.com>
2024-07-04 00:11:49 +0800

    add has0a relation example. 14.1

---
+++ primer_plus/chap14/14_1/14_1_1/valarray.cpp
@@ -0,0 +1,10 @@
+/*#include <valarray>
+
+valarray<int> q_values;
+valarray<double> weights;
+double gpa[5] = {3.1, 3.5, 3.8, 2.9, 3.3};
+valarray<double> v1;
+valarray<int> v2(8);
+valarray<int> v3(10, 8);
+valarray<double> v4(gpa, 4)
+*/
