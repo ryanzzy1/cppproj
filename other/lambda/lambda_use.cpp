6a4777b7f55b73b79a779e6af1ad2a1d365bc90a
ryan <ryan_zhang310@163.com>
2024-07-17 10:11:13 +0800

    add source address.

--- other/lambda/lambda_use.cpp
+++ other/lambda/lambda_use.cpp
@@ -1,3 +1,5 @@
+// code soure:
+// https://developer.aliyun.com/article/1567533?spm=a2c6h.14164896.0.0.34f847c5MRvmOe&scm=20140722.S_community@@%E6%96%87%E7%AB%A0@@1567533._.ID_1567533-RL_C-LOC_search~UND~community~UND~item-OR_ser-V_3-P0_1
 #include <iostream>
 #include <type_traits>
 
@@ -18,3 +20,4 @@ int main()
     demo();
     return 0;
 }
+
