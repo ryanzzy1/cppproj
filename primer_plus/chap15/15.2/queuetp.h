11e4e6c2c4d1a4c07f459f06db9613ecc8bab3e2
ryan <ryan_zhang310@163.com>
2024-08-30 17:58:19 +0800

    add class definition.

--- primer_plus/chap15/15.2/queuetp.h
+++ primer_plus/chap15/15.2/queuetp.h
@@ -1,3 +1,32 @@
 // queuetp.h -- queue template with a nested class
 #ifndef QUEUETP_H_
 #define QUEUETP_H_ 
+
+template <class Item>
+class QueueTP
+{
+private:
+    enum {Q_SIZE = 10};
+    // Node is a nested class definition
+    class Node
+    {
+    public:
+        Item item;
+        Node * next;
+        Node(const Item & i) : item(i), next(0) {}
+    };
+    Node * front;
+    Node * rear;
+    int items;
+    const int qsize;
+    QueueTP(const QueueTP & q) : qsizee(0) {}
+    QueueTP & operator=(const QueueTP & q) {return *this;}
+public:
+    QueueTP(int qs = Q_SIZE);
+    ~QueueTP();
+};
+
+
+
+
+#endif 
