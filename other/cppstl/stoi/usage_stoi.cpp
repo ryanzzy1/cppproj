d2cfca824b2290392ed10c84fe0352f24840cb8b
ryan <ryan_zhang310@163.com>
2024-09-23 17:56:47 +0800

    add stoi usage eg.

---
+++ other/cppstl/stoi/usage_stoi.cpp
@@ -0,0 +1,27 @@
+// stoi example
+// 转自原链接：https://www.cnblogs.com/tsruixi/p/12944470.html
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str_dec = "2001, A Space Odyssey";
+    std::string str_hex = "40c3";
+    std::string str_bin = "-100101100001";
+    std::string str_auto = "0x7f";
+
+    std::string::size_type sz;
+
+    int i_dec = std::stoi(str_dec, &sz);
+    int i_hex = std::stoi(str_hex, nullptr, 16);
+    int i_bin = std::stoi(str_bin, nullptr, 2);
+    int i_auto = std::stoi(str_auto, nullptr, 0);
+
+    std::cout << str_dec << ": " << i_dec << " and [" << str_dec.substr(sz) << "]\n";
+    std::cout << str_hex << ": " << i_hex << '\n';
+    std::cout << str_bin << ": " << i_bin << '\n';
+    std::cout << str_auto << ": " << i_auto << '\n';
+
+    return 0;  
+}
